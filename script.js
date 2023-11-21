b7966362abc64427ec648ab231b867dafb31cd4b
William Sommerhauser <wssommerhauser@gmail.com>
2023-11-20 20:14:51 -0600

    alert popups working

--- script.js
+++ script.js
@@ -23,11 +23,25 @@ window.addEventListener("load", function() {
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
         
-        //would check for blank inputs. (?)
-        /*if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoLevel.value === "") {
-            alert("All fields are required!");
+        if (validateInput(pilot) === "Is a Number" || validateInput(pilot) === "Empty"){
+            alert("pilot name must be a string.");
             event.preventDefault();
-        }*/
+        }
+        else if (validateInput(copilot) === "Is a Number" || validateInput(copilot) === "Empty"){
+            alert("copilot name must be a string.");
+            event.preventDefault();
+        }
+        else if (validateInput(fuelLevel) === "Not a Number" || validateInput(fuelLevel) === "Empty"){
+            alert("fuel level must be a number.");
+            event.preventDefault();
+        }
+        else if (validateInput(cargoLevel) === "Not a Number" || validateInput(cargoLevel) === "Empty"){
+            alert("cargo name must be a number.");
+            event.preventDefault();
+        }
+       else{
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+       }
      })
+     
  });
